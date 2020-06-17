4c08d58694ee74ba8f19556e14fabc88e30e0357
RikySaepuloh <dikysaepuloh@gmail.com>
2020-06-17 13:22:15 +0700

    Initial Commit V.2

---
+++ app/src/main/java/com/saku/portalsatpam/NeinActivity.kt
@@ -0,0 +1,271 @@
+package com.saku.portalsatpam
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.bumptech.glide.Glide
+import com.saku.portalsatpam.adapter.FragmentAdapter
+import com.saku.portalsatpam.fragments.FragmentKeperluan
+import kotlinx.android.synthetic.main.activity_nein.*
+
+
+class NeinActivity : AppCompatActivity(), DataPasserKeperluan {
+    private var mAdapter: FragmentAdapter? = null
+    var keperluan : String? = null
+    var tujuan : String? = null
+    var penghuni : String? = null
+    var imgPath : String? = null
+    var stat = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nein)
+
+        val mMessageReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent) {
+                if(intent.hasExtra("penghuni")){
+                    val name = intent.getStringExtra("penghuni")
+//                    Toast.makeText(this@NeinActivity, name, Toast.LENGTH_SHORT).show()
+                    penghuni = name
+                    tv_penghuni.text = penghuni
+                    resetBG()
+                    card_identitas.background = ContextCompat.getDrawable(this@NeinActivity,R.drawable.content_menu_selected)
+                    n4.setImageResource(R.drawable.n4_true)
+                    stat++
+                }else if(intent.hasExtra("imagefile")){
+                    val image = intent.getStringExtra("imagefile")
+                    imgPath = image
+                    Toast.makeText(this@NeinActivity, image, Toast.LENGTH_SHORT).show()
+                    Glide.with(this@NeinActivity).load(image).into(identitas)
+                    supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentSelesai()).commitAllowingStateLoss();
+                    card_keperluan.isEnabled = false
+                    card_tujuan.isEnabled = false
+                    card_penghuni.isEnabled = false
+                    card_identitas.isEnabled = false
+                }else if(intent.hasExtra("back")){
+                    resetBG()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentPenghuni()).commitAllowingStateLoss();
+                    card_penghuni.background = ContextCompat.getDrawable(this@NeinActivity,R.drawable.content_menu_selected)
+                    n4.setImageResource(R.drawable.n4_false)
+                    stat--
+                }
+            }
+        }
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(mMessageReceiver, IntentFilter("message_subject_intent"))
+
+        val fragment : FragmentKeperluan = FragmentKeperluan()
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, fragment).commit();
+
+//        val mesrec: BroadcastReceiver = object : BroadcastReceiver() {
+//            override fun onReceive(context: Context?, intent: Intent) {
+//                if(intent.hasExtra(""))
+//                val name = intent.getStringExtra("imagefile")
+//                Toast.makeText(this@NeinActivity, name, Toast.LENGTH_SHORT).show()
+////                penghuni = name
+////                tv_penghuni.text = penghuni
+////                resetBG()
+////                card_identitas.background = ContextCompat.getDrawable(this@NeinActivity,R.drawable.content_menu_selected)
+////                n4.setImageResource(R.drawable.n4_true)
+////                stat++
+//            }
+//        }
+//        LocalBroadcastManager.getInstance(this)
+//            .registerReceiver(mesrec, IntentFilter("imagefile_intent"))
+
+//        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+//        supportFragmentManager.beginTransaction().replace(R.id.viewpager, FragmentKeperluan.newInstance()).commit();
+
+//        val fragments: ArrayList<Fragment> =
+//            arrayListOf(
+//                FragmentKeperluan()
+////                SearchFragment(),
+////                AddFragment(),
+////                NotificationsFragment(),
+////                ProfileFragment()
+//            )
+//        viewpager.adapter = FragmentViewPagerAdapter(supportFragmentManager, fragments)
+////        viewpager.offscreenPageLimit = 5
+//        viewpager.currentItem = 0
+
+
+//        viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+//            override fun onPageScrollStateChanged(state: Int) {
+//
+//            }
+//
+//            override fun onPageScrolled(
+//                position: Int,
+//                positionOffset: Float,
+//                positionOffsetPixels: Int
+//            ) {
+//
+//            }
+//
+//            override fun onPageSelected(position: Int) {
+//                when(position){
+//                    0 -> navigation.selectedItemId = position
+//                    1 -> navigation.selectedItemId = position
+//                    2 -> navigation.selectedItemId = position
+//                    3 -> navigation.selectedItemId = position
+//                    4 -> navigation.selectedItemId = position
+//
+//                }
+//
+//
+//            }
+//        })
+//        navigation.setOnNavigationItemSelectedListener { it
+//            it.let {
+//                when(it.itemId){
+//                    R.id.nav_home -> {
+//                        binding.viewpager.currentItem = 0
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_search-> {
+//                        binding.viewpager.currentItem = 1
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_add -> {
+//                        binding.viewpager.currentItem = 2
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_notifications -> {
+//                        binding.viewpager.currentItem = 3
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_profile -> {
+//                        binding.viewpager.currentItem = 4
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                    else -> {
+//                        binding.viewpager.currentItem = 0
+//                        return@setOnNavigationItemSelectedListener true
+//                    }
+//                }
+//            }
+
+
+        card_keperluan.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+        card_keperluan.setOnClickListener{
+            resetBG()
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentKeperluan()).commit();
+            card_keperluan.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+            n1.setImageResource(R.drawable.n1_true)
+        }
+        card_tujuan.setOnClickListener{
+            if(keperluan=="empty"){
+                Toast.makeText(this,"Isi menu ini terlebih dahulu",Toast.LENGTH_LONG).show()
+            }else{
+                supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentTujuan()).commit();
+                resetBG()
+                card_tujuan.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+                n2.setImageResource(R.drawable.n2_true)
+                stat++
+            }
+        }
+        card_penghuni.setOnClickListener{
+            if(stat>1){
+                resetBG()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentPenghuni()).commit();
+                card_penghuni.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+                n3.setImageResource(R.drawable.n3_true)
+                stat++
+            }else{
+                Toast.makeText(this,"Isi menu ini terlebih dahulu",Toast.LENGTH_LONG).show()
+            }
+        }
+        card_identitas.setOnClickListener{
+            if(stat>2){
+                resetBG()
+                card_identitas.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+                n4.setImageResource(R.drawable.n4_true)
+            }else{
+                Toast.makeText(this,"Isi menu ini terlebih dahulu",Toast.LENGTH_LONG).show()
+            }
+        }
+        back.setOnClickListener {
+            val intent = Intent(this@NeinActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+//            //vibrate(longArrayOf(0, 350))
+//            val intent = Intent(this@NeinActivity,MainActivity::class.java)
+//            startActivity(intent)
+        }
+
+    }
+
+    fun changeFragment(fragmentClass: Fragment) {
+        var fragment: Fragment? = null
+        try {
+            fragment = fragmentClass
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        // Insert the fragment by replacing any existing fragment
+        val fragmentManager: FragmentManager = supportFragmentManager
+        if (fragment != null) {
+            fragmentManager.beginTransaction().replace(R.id.frame_layout, fragment)
+                .commit()
+        }
+    }
+
+//    fun changeFragment(myfragment : Fragment){
+//        supportFragmentManager.beginTransaction().add(R.id.frame_layout, myfragment).commit();
+//    }
+
+    fun resetBG(){
+//        n1.setImageResource(R.drawable.n1_false)
+//        n2.setImageResource(R.drawable.n2_false)
+//        n3.setImageResource(R.drawable.n3_false)
+//        n4.setImageResource(R.drawable.n4_false)
+        card_keperluan.setBackgroundResource(0)
+        card_penghuni.setBackgroundResource(0)
+        card_tujuan.setBackgroundResource(0)
+        card_identitas.setBackgroundResource(0)
+    }
+
+
+
+    override fun onBackPressed() {
+        val intent = Intent(this@NeinActivity,MainActivity::class.java)
+        startActivity(intent)
+//        super.onBackPressed()
+    }
+
+    override fun onDataPasserKeperluan(data: String, untuk: String) {
+        if(untuk=="keperluan"){
+            keperluan = data
+            tv_keperluan.text = keperluan
+            resetBG()
+            card_tujuan.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+            n2.setImageResource(R.drawable.n2_true)
+            stat++
+        }else if (untuk=="tujuan"){
+            tujuan = data
+            tv_tujuan.text = tujuan
+            resetBG()
+            card_penghuni.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+            n3.setImageResource(R.drawable.n3_true)
+            stat++
+        }
+//        else if (untuk=="penghuni"){
+//            penghuni = data
+//            tv_penghuni.text = penghuni
+////            card_penghuni.background = ContextCompat.getDrawable(this,R.drawable.content_menu_selected)
+////            n3.setImageResource(R.drawable.n3_true)
+////            stat++
+//        }else{
+//            tujuan = data
+//            tv_tujuan.text = tujuan
+//        }
+    }
+}
---
+++ app/src/main/java/com/saku/portalsatpam/fragments/FragmentKeperluan.kt
@@ -0,0 +1,134 @@
+package com.saku.portalsatpam.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.saku.portalsatpam.DataPasserKeperluan
+import com.saku.portalsatpam.FragmentTujuan
+import com.saku.portalsatpam.NeinActivity
+import com.saku.portalsatpam.R
+import kotlinx.android.synthetic.main.fragment_menu_keperluan.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentKeperluan.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentKeperluan : Fragment() {
+    private lateinit var myview: View
+    private lateinit var mycontext : Context
+    lateinit var dataPasser: DataPasserKeperluan
+
+//    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+//
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        myview = inflater.inflate(R.layout.fragment_menu_keperluan, container, false)
+        mycontext = context!!
+        return myview
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        dataPasser = context as DataPasserKeperluan
+    }
+
+    fun passData(data: String){
+        dataPasser.onDataPasserKeperluan(data,"keperluan")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if((activity as NeinActivity).keperluan!="-"){
+            when((activity as NeinActivity).keperluan){
+                "Paket" ->{
+                    myview.card_paket.isPressed = true
+                }
+                "Makanan" ->{
+                    myview.card_makanan.isPressed = true
+                }
+                "OJOL" ->{
+                    myview.card_ojek.isPressed = true
+                }
+                "Tamu" ->{
+                    myview.card_tamu.isPressed = true
+                }
+                "Jualan" ->{
+                    myview.card_jualan.isPressed = true
+                }
+                "Teknisi" ->{
+                    myview.card_teknisi.isPressed = true
+                }
+            }
+//            myview..setText((activity as NeinActivity).tujuan)
+        }
+        myview.card_makanan.setOnClickListener {
+            passData("Makanan")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+        }
+        myview.card_jualan.setOnClickListener {
+            passData("Jualan")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+        }
+        myview.card_ojek.setOnClickListener {
+            passData("OJOL")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+        }
+        myview.card_paket.setOnClickListener {
+            passData("Paket")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+
+        }
+        myview.card_tamu.setOnClickListener {
+            passData("Tamu")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+        }
+        myview.card_teknisi.setOnClickListener {
+            passData("Teknisi")
+            (activity as NeinActivity?)?.changeFragment(FragmentTujuan())
+        }
+
+    }
+
+        companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentKeperluan.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FragmentKeperluan().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
